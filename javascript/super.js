b5402263f29318e61583515d02225879a7df360f
Ray <55176622+kdl9405@users.noreply.github.com>
2021-02-04 22:35:07 +0900

    0204JS

--- javascript/super.js
+++ javascript/super.js
@@ -1,3 +1,31 @@
+class Circle {
+    constructor(radius) {
+      this.radius = radius; // 반지름
+    }
+  
+    // 원의 지름
+    getDiameter() {
+      return 2 * this.radius;
+    }
+  
+    // 원의 둘레
+    getPerimeter() {
+      return 2 * Math.PI * this.radius;
+    }
+  
+    // 원의 넓이
+    getArea() {
+      return Math.PI * Math.pow(this.radius, 2);
+    }
+  }
+  
+  // 자식 클래스
+  
+  
+  // 반지름이 2, 높이가 10인 원통
+  const cylinder = new Cylinder(10);
+  console.log(cylinder.getArea());
+
 
   class Foo {
     x = 1;            // Field declaration
