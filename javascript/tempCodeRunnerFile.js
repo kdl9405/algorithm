83c4edc416e36da0aeb52a6d33961fbaca5f51d3
Ray <55176622+kdl9405@users.noreply.github.com>
2021-02-08 15:04:24 +0900

    0207JS

--- javascript/tempCodeRunnerFile.js
+++ javascript/tempCodeRunnerFile.js
@@ -1,7 +1,3 @@
-let N; // 정수를 저장할 변수
-rl.on("line", function(line){
-    console.log(`print input string ${line}`);
-    N = Number(line);
-    console.log(`print input number ${N}`);
-    rl.close();
-})
+let input = '1';
+
+console.log(Number(input));
