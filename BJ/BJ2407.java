4e8e0ec65867c587865202cdbe3312a312202e2b
Ray <55176622+kdl9405@users.noreply.github.com>
2021-02-18 23:39:24 +0900

    BJ0218

---
+++ BJ/BJ2407.java
@@ -0,0 +1,45 @@
+package BJ;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * 조합
+ */
+public class BJ2407 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringTokenizer st = new StringTokenizer(sc.nextLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        arr = new BigInteger[n + 1][n + 1];
+
+        System.out.println(nCr(n, m));
+    }
+
+    static BigInteger[][] arr;
+
+    static BigInteger nCr(int n, int r) {
+
+        if (r == 1) {
+            return BigInteger.valueOf((long)n);
+        }
+
+        if (r == 0 || r == n) {
+            return BigInteger.valueOf(1);
+        }       
+
+        if (arr[n][r] != null) {
+            return arr[n][r];
+        }
+
+        arr[n][r] = nCr(n - 1, r - 1).add(nCr(n - 1, r));
+       
+
+        return arr[n][r];
+    }
+}
