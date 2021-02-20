3f6213886f2fa33082414b275ff091e88b44d204
Ray <55176622+kdl9405@users.noreply.github.com>
2021-02-20 16:13:14 +0900

    BJ 2020

---
+++ BJ/BJ13549.java
@@ -0,0 +1,89 @@
+package BJ;
+
+import java.io.*;
+import java.util.*;
+
+/* 
+    숨바꼭질 3
+*/
+public class BJ13549 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int time = Math.abs(k - n);
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+        queue.offer(n);
+        int count = 0;
+
+        loop: while (true) {
+
+            if (count >= time) {
+                break;
+            }
+
+            HashSet<Integer> temp = new HashSet<>();
+
+            while (!queue.isEmpty()) {
+                int current = queue.poll();
+
+                temp.add(current);
+
+                if (current != 0) {
+                    for (int i = current; i <= k + time; i *= 2) {
+
+                        if (!visit[i]) {
+
+                            visit[i] = true;
+
+                            temp.add(i);
+
+                            if (i == k) {
+                                break loop;
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            System.out.println("temp " + temp);
+
+            count++;
+
+            HashSet<Integer> next = new HashSet<>();
+
+            for (Integer q : temp) {
+
+                if (q - 1 == k || q + 1 == k) {
+                    break loop;
+                }
+                if (q - 1 > 0 && !visit[q - 1]) {
+                    next.add(q - 1);
+                    visit[q - 1] = true;
+                }
+                if (!visit[q + 1]) {
+                    next.add(q + 1);
+                    visit[q + 1] = true;
+
+                }
+            }
+
+            queue.addAll(next);
+            System.out.println("queue " + queue);
+
+        }
+
+        System.out.println(count);
+
+    }
+
+    static boolean[] visit = new boolean[2000002];
+
+}
---
+++ BJ/BJ1865.java
@@ -0,0 +1,118 @@
+package BJ;
+
+import java.io.*;
+import java.util.*;
+
+/* 
+    웜홀 
+*/
+
+public class BJ1865 {
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+
+            roads = new ArrayList<>();
+            for(int i = 0; i<=n; i++){
+                roads.add(new ArrayList<>());
+            }
+
+            while (m-- > 0) {
+                st = new StringTokenizer(br.readLine());
+
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                int t = Integer.parseInt(st.nextToken());
+
+                roads.get(s).add(new Road(e,t));
+                roads.get(e).add(new Road(s,t));
+            }
+
+            while (w--> 0) {
+                st = new StringTokenizer(br.readLine());
+
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                int t = Integer.parseInt(st.nextToken());
+
+                roads.get(s).add(new Road(e,-t));
+
+            }
+            
+
+            if (bellmanFord(n)) {
+                sb.append("YES").append("\n");
+            }else{
+                sb.append("NO").append("\n");
+            }
+
+        }
+
+        System.out.println(sb.toString());
+
+    }
+
+    static List<ArrayList<Road>> roads;
+    static int[] times;
+    static int INF = 9000000;
+
+    static boolean bellmanFord(int n){
+
+        times = new int[n+1];
+        Arrays.fill(times, INF);
+
+        times[1] = 0;
+        boolean update = false;
+
+        for(int i = 1; i<n; i++){
+            update = false;
+
+            for(int j = 1; j<=n; j++){
+                for(Road road : roads.get(j)){
+                    if (times[road.to] > times[j]+road.time) {
+                        times[road.to] = times[j]+road.time;
+                        update = true;
+                    }
+                }
+            }
+
+            if (!update) {
+               break;
+            }
+        }
+
+        if (update) {
+            for(int i = 1; i<=n; i++){
+                for(Road road : roads.get(i)){
+                    if (times[road.to] > times[i]+road.time) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
+
+
+class Road{
+    int to;
+    int time;
+
+    public Road(int to, int time){
+        this.to = to;
+        this.time = time;
+    }
+}
