e4df6c10f7dac6c90fe32b40d6544f027d95316b
Ray <55176622+kdl9405@users.noreply.github.com>
2021-02-22 15:40:53 +0900

    BJ2021

---
+++ BJ/BJ14501.java
@@ -0,0 +1,30 @@
+package BJ;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BJ14501 {
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        rr = new int[n][2];
+
+        for(int i = 0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+
+    }
+
+    static int[][] arr;
+
+    static 
+}
