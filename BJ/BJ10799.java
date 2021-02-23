6fbaa699de7781827fb0f244e2a9676bf2d3e64d
Ray <55176622+kdl9405@users.noreply.github.com>
2021-02-23 23:29:49 +0900

    BOJ 0223

---
+++ BJ/BJ10799.java
@@ -0,0 +1,47 @@
+package BJ;
+
+import java.util.*;
+
+/* 
+    쇠막대기
+*/
+public class BJ10799 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        Deque<Character> deque = new LinkedList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == ')' && deque.peekLast() == '(') {
+
+                deque.pollLast();
+                deque.addLast('*');
+
+            } else {
+                deque.addLast(s.charAt(i));
+            }
+        }
+
+        Stack<Character> stack = new Stack<>();
+        int count = 0;
+
+        while (!deque.isEmpty()) {
+            char c = deque.pollFirst();            
+            if (c == '*') {
+                count += stack.size();
+            }else if (c == '(') {                
+                stack.push(c);
+            }else {
+                stack.pop();
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
---
+++ BJ/BJ2346.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+/* 
+    풍선 터뜨리기
+
+    
+*/
+
+public class BJ2346 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Deque<Integer[]> deque = new ArrayDeque<>();
+        for (int i = 1; i <= n; i++) {
+            deque.add(new Integer[] { i, Integer.parseInt(st.nextToken()) });
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int x = 1;
+        while (!deque.isEmpty()) {
+
+
+            Integer[] balloon;
+
+            if (x > 0) {
+                while (x-- > 1) {
+                    deque.addLast(deque.pollFirst());
+                }
+                balloon = deque.pollFirst();
+            } else {
+                while (x++ < -1) {
+                    deque.addFirst(deque.pollLast());
+                }
+                balloon = deque.pollLast();
+            }
+
+            sb.append(balloon[0]).append(" ");
+            x = balloon[1];
+
+        }
+
+        System.out.println(sb.toString());
+
+    }
+}
