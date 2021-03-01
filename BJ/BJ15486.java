ee9f2c870ae14a3ebbb121f5c7663d7a37334e59
Ray <55176622+kdl9405@users.noreply.github.com>
2021-03-02 01:52:24 +0900

    BOJ 0301

--- BJ/BJ15486.java
+++ BJ/BJ15486.java
@@ -1,45 +1,42 @@
 package BJ;
 
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 /* 
-    퇴사
+    퇴사 2
 */
 
-public class BJ14501 {
-    
+public class BJ15486 {
+
     public static void main(String[] args) throws NumberFormatException, IOException {
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
 
-        arr = new int[n+2][2];
+        int[][] arr = new int[n + 2][2];
 
-        for(int i = 1; i<=n; i++){
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-
             arr[i][0] = Integer.parseInt(st.nextToken());
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        dp = new int[n+6];
-
+        int[] dp = new int[n + 2];
         int sum = 0;
-        for(int i = 1; i<=n+1; i++){
+
+        for (int i = 1; i <= n+1; i++) {
             dp[i] = Math.max(dp[i], sum);
 
-            dp[arr[i][0]+ i] = Math.max(dp[arr[i][0]+ i], dp[i]+arr[i][1]);
+            if (arr[i][0] + i <= n+1) {
+                dp[arr[i][0] + i] = Math.max(dp[arr[i][0] + i], arr[i][1] + dp[i]);
+            }
 
-            sum = Math.max(dp[i], sum);
+            sum = Math.max(sum, dp[i]);
         }
 
         System.out.println(sum);
-
     }
 
-    static int[][] arr;
-    static int[] dp; // dp[i] = i일까지의 최대 금액 (i)
 }
