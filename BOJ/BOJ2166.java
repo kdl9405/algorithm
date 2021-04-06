846c459731b4be937de82d05aecbd7ff8468f51d
Ray <55176622+kdl9405@users.noreply.github.com>
2021-04-06 17:34:10 +0900

    0406 afternoon BOJ

---
+++ BOJ/BOJ2166.java
@@ -0,0 +1,40 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ2166 {
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[][] points = new long[N+1][2];
+
+        for(int i= 0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            
+            points[i][0] = Integer.parseInt(st.nextToken());
+            points[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        points[N][0] = points[0][0];
+        points[N][1] = points[0][1];
+
+        long x = 0;
+        long y = 0;
+
+        for(int i = 0; i<N; i++){
+            x += (points[i][0]*points[i+1][1]);
+            y += (points[i][1]*points[i+1][0]);
+        }
+
+        double answer = Math.abs(x-y);
+        answer /=2.0;
+
+        System.out.println(String.format("%.1f", answer));
+    }
+}
---
+++ kakao/pro4.java
@@ -0,0 +1,12 @@
+package kakao;
+
+import java.io.*;
+import java.util.*;
+
+public class pro4 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
