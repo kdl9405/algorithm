1d832bf806700510c77c3c0f4abeee3c84356a48
Ray <55176622+kdl9405@users.noreply.github.com>
2021-04-13 14:52:22 +0900

    package setting

---
+++ programmers/pro180.java
@@ -0,0 +1,13 @@
+package programmers;
+
+public class pro180 {
+    public static void main(String[] args) {
+
+        
+    }
+
+    public static int solution(int n, int[] cores) {
+        int answer = 0;
+        return answer;
+    }
+}
