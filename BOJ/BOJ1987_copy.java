47fd1d033c30d9e14a47192d375f9c71d3724aae
Ray <55176622+kdl9405@users.noreply.github.com>
2021-04-14 17:23:08 +0900

    0414 afternoon tistory

--- BOJ/BOJ1987_copy.java
+++ BOJ/BOJ1987_copy.java
@@ -10,10 +10,10 @@ import java.util.*;
 */
 public class BOJ1987_copy {
 
-    static int count, R, C;
-    static HashSet<Character> alphabet;
-    static char[][] board;
-    static int[][] direction = {{-1,0}, {1,0},{0,-1},{0,1}};
+    static int max, R, C;
+    static HashSet<Character> alphabet; // 사용한 알파벳을 저장
+    static char[][] board; 
+    static int[][] direction = {{-1,0}, {1,0},{0,-1},{0,1}}; // 상하좌우 이동
 
     
     public static void main(String[] args) throws IOException {
@@ -29,40 +29,36 @@ public class BOJ1987_copy {
             board[i] = br.readLine().toCharArray();
         }
 
-        System.out.println(1<<'D'-'A');
-
-        count = 0;
+        max = 0;  // 최대 이동 칸의 수
         alphabet = new HashSet<>();
-        alphabet.add(board[0][0]);
+        alphabet.add(board[0][0]); // 시작 칸의 알파벳을 저장
         dfs(0, 0);
 
-        System.out.println(count);
-
-
+        System.out.println(max);
     }
 
     static void dfs(int r, int c){
 
-        boolean possible = false;
+        boolean possible = false;  // 다음 이동이 가능한지 체크
 
         for(int i = 0; i<4; i++){
             int nr = r+direction[i][0];
             int nc = c+direction[i][1];
 
-            if (nr>=0 && nr<R && nc>=0 && nc<C) {
-                if (alphabet.contains(board[nr][nc])) {
+            if (nr>=0 && nr<R && nc>=0 && nc<C) {  // 다음 이동 좌표가 볌위안에 존재하는지 
+                if (alphabet.contains(board[nr][nc])) { // 다음 좌표의 알파벳을 이미 지나왔는지
                     continue;
                 }
-                possible = true;
+                possible = true; // 다음 이동 가능
 
-                alphabet.add(board[nr][nc]);
+                alphabet.add(board[nr][nc]); // 알파벳을 추가하고 DFS
                 dfs(nr, nc);
-                alphabet.remove(board[nr][nc]);
+                alphabet.remove(board[nr][nc]); // 추가한 알파벳 제거
             }
         }
 
-        if (!possible) {
-            count = Math.max(count, alphabet.size());
+        if (!possible) {  // 더 이상 이동이 불가한 경우, 이동 거리를 갱신
+            max = Math.max(max, alphabet.size());
             return;
         }
     }
--- BOJ/BOJ1987_copy2.java
+++ BOJ/BOJ1987_copy2.java
@@ -10,10 +10,10 @@ import java.util.*;
 */
 public class BOJ1987_copy2 {
 
-    static int count, R, C;
+    static int max, R, C;
     static char[][] board;
-    static boolean[] visit;
-    static int[][] direction = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+    static boolean[] visit;  // 사용한 알파벳을 boolean값으로 저장
+    static int[][] direction = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };  // 상하좌우 이동
 
     public static void main(String[] args) throws IOException {
         
@@ -28,16 +28,14 @@ public class BOJ1987_copy2 {
             board[i] = br.readLine().toCharArray();
         }
 
-        visit = new boolean[26];
-        visit[board[0][0]-'A'] = true;
+        visit = new boolean[26];  // 알파벳의 수만큼 할당
+        visit[board[0][0]-'A'] = true;  // 시작 칸의 알파벳을 체크
 
-        count = 0;
+        max = 0;
        
-        dfs(0, 0, 1);
+        dfs(0, 0, 1); // 줄, 칸, 이동거리
 
-        System.out.println(count);
-
-
+        System.out.println(max);
     }
 
     static void dfs(int r, int c, int d){
@@ -46,16 +44,14 @@ public class BOJ1987_copy2 {
             int nr = r + direction[i][0];
             int nc = c + direction[i][1];
 
-            if (nr>=0 && nr<R && nc>=0 && nc<C) {
-                if (!visit[board[nr][nc]-'A']) {
+            if (nr>=0 && nr<R && nc>=0 && nc<C) {  // 다음 이동 칸이 범위 안에 있다면
+                if (!visit[board[nr][nc]-'A']) { // 이미 사용된 알파벳이 아니라면
                     visit[board[nr][nc]-'A'] = true;
-                    dfs(nr, nc, d+1);
+                    dfs(nr, nc, d+1); // 거리를 추가하고 dfs 진행
                     visit[board[nr][nc]-'A'] = false;
                 }                
             }
         }
-
-        count = Math.max(count, d);
-        
+        max = Math.max(max, d); // 이동거리 갱신
     }
 }
