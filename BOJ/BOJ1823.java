ab694ca762aef68355f5591c18189c6c7d94586a
Ray <55176622+kdl9405@users.noreply.github.com>
2021-04-14 16:38:14 +0900

    0414 afternoon tistory

--- BOJ/BOJ1823.java
+++ BOJ/BOJ1823.java
@@ -1,7 +1,6 @@
 package BOJ;
 
 import java.io.*;
-import java.util.*;
 
 /* 
     수확 
@@ -29,24 +28,24 @@ public class BOJ1823 {
 
     static int n;
     static int[] arr;   
-    static int[][] dp; // i에서 j가 남아있을 때의 최대 수확량
+    static int[][] dp; // i에서 j범위의 벼가 남았을 때의 최대 가치
 
     static int findDP(int start, int end){
         
-        if (start > end) {
+        if (start > end) { // 모두 수확한 경우
             return 0;
         }
 
-        if (dp[start][end] != 0) {
+        if (dp[start][end] != 0) { // 이미 계산된 최대 가치가 존재하는 경우
             return dp[start][end];
         }
 
-        int index = n-(end-start);
+        int index = n-(end-start); // 현재 수확 순서
 
+        // Start~End 벼가 있을 때의 최대가치 
+        //              = Max(왼쪽 벼를 수확했을 때 X 왼쪽 벼의 수확가치, 오른쪽 벼를 수확했을 때 X 오른쪽 벼의 수확가치)   
         dp[start][end] = Math.max(findDP(start+1, end)+(arr[start]*index), findDP(start, end-1)+(arr[end]*index));
 
         return dp[start][end];
     }
-   
-
 }
--- BOJ/BOJ1823_copy.java
+++ BOJ/BOJ1823_copy.java
@@ -1,7 +1,6 @@
 package BOJ;
 
 import java.io.*;
-import java.util.*;
 
 /* 
     수확 
@@ -17,14 +16,14 @@ public class BOJ1823_copy {
 
         int n = Integer.parseInt(br.readLine());
 
-        arr = new int[n + 1];
+        arr = new int[n + 1]; // 벼
 
         for (int i = 1; i <= n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
         max = 0;
-        sum = new int[n + 1];
+        cost = new int[n + 1];
 
         dfs(1, 1, n);
 
@@ -32,24 +31,26 @@ public class BOJ1823_copy {
     }
 
     static int[] arr;   
-    static int[] sum;
+    static int[] cost; // 수확 시기때마다 수확한 벼의 가치
     static int max;
 
     static void dfs(int index, int left, int right){
-        if (left == right) {
+        if (left == right) { // 벼가 1개 남은 경우
             int t = 0;
-            for(int i = 1; i<sum.length-1; i++){
-                t +=(i*sum[i]);
+            for(int i = 1; i<cost.length-1; i++){
+                t +=(i*cost[i]);
             }
             t += (index*arr[left]);
             max = Math.max(max, t);
             return; 
         }       
 
-        sum[index] = arr[left];
+        // 왼쪽 끝의 벼를 수확하는 경우
+        cost[index] = arr[left];
         dfs(index+1, left+1, right);
 
-        sum[index] = arr[right];
+        // 오른쪽 끝의 벼를 수확하는 경우
+        cost[index] = arr[right];
         dfs(index+1, left, right-1);
     }
 
