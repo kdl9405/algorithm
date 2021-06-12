5d9227e0a4e2040d2381ba1e42df78210ac138f2
Ray <55176622+kdl9405@users.noreply.github.com>
2021-06-12 16:24:22 +0900

    0612 afternoon BOJ

---
+++ BOJ/BOJ5014.java
@@ -0,0 +1,52 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ5014 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int F = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int G = Integer.parseInt(st.nextToken());
+        int U = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+
+        int[] min = new int[F + 1];
+        Arrays.fill(min, -1);
+        min[S] = 0;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(S);
+
+        while (!queue.isEmpty()) {
+            int now = queue.poll();
+
+            if (now == G) {
+                break;
+            }
+
+            if (now + U <= F && min[now + U] == -1) {
+                min[now + U] = min[now] + 1;
+                queue.add(now + U);
+            }
+
+            if (0 < now - D && min[now - D] == -1) {
+                min[now - D] = min[now] + 1;
+                queue.add(now - D);
+            }
+
+        }
+
+        if (min[G] == -1) {
+            System.out.println("use the stairs");
+        }else{
+            System.out.println(min[G]);
+        }
+
+    }
+}
