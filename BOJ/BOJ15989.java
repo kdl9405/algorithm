f1451e86ce7fc9d05827c570e5d003258dc4ffb2
Ray <55176622+kdl9405@users.noreply.github.com>
2021-09-03 00:00:11 +0900

    0902

---
+++ BOJ/BOJ15989.java
@@ -0,0 +1,71 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+    1, 2, 3 더하기 4
+*/
+
+public class BOJ15989 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        dp = new int[10001][4];
+
+        while (T-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+
+            sb.append(findN(n, 0)).append("\n");
+
+
+        }
+
+        System.out.println(sb.toString().trim());
+        
+    }
+
+    static int[][] dp;
+
+    static int findN ( int n, int x ){
+
+        if (n < 0) {
+            return 0;
+        }
+
+        if (n == 0 && x != 3) {
+            return 1;
+        }
+
+        if (dp[n][x] != 0) {
+            return dp[n][x];
+        }
+
+        if (x == 3) {        
+            if (n % 3 == 0 && n / 3 > 0) {
+                return 1;
+            }else {
+                return 0;
+            }
+        }
+
+        if (x == 2) {
+            
+            return dp[n][x] = findN(n-2, 2) + findN(n-2, 3);
+        }
+
+        if (x == 1) {
+            return dp[n][x] =  findN(n-1, 1) + findN(n-1, 2) + findN(n-1, 3);
+        }
+
+        return dp[n][0] = findN(n, 1) + findN(n, 2) + findN(n, 3);
+
+    }
+}
---
+++ BOJ/BOJ3613.java
@@ -0,0 +1,60 @@
+package BOJ;
+
+import java.io.*;
+
+/* 
+    Java vs C++
+*/
+public class BOJ3613 {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String parameter = br.readLine();
+
+        boolean isJava = false;
+        boolean isCpp = false;
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(parameter.charAt(0));
+
+
+        if (!Character.isLowerCase(parameter.charAt(0)) || parameter.charAt(parameter.length()-1) == '_') {
+            isJava = true;
+            isCpp = true;
+        }else{
+
+            for(int i = 1; i<parameter.length(); i++){
+
+                char c = parameter.charAt(i);
+
+                if (Character.isUpperCase(c)) {
+                    isJava = true;
+                    sb.append('_').append(Character.toLowerCase(c));
+                }else if(c == '_'){
+                    isCpp = true;
+                    i++;
+                    c = parameter.charAt(i);
+                    if (!Character.isLowerCase(c)) {
+                        isJava = true;
+                        break;
+                    }
+                    sb.append(Character.toUpperCase(c));
+                }else{
+                    sb.append(c);
+                }
+            }
+        }
+
+
+        if (isJava && isCpp) {
+            System.out.println("Error!");
+        }else{
+            System.out.println(sb.toString());
+        }
+
+
+        
+    }
+}
