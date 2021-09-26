e59b055978d8a39988475a4d93e797ca31c56bdf
Ray <55176622+kdl9405@users.noreply.github.com>
2021-09-26 23:02:12 +0900

    0926

---
+++ BOJ/BOJ17521.java
@@ -0,0 +1,52 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+/* 
+    Byte Coin
+*/
+public class BOJ17521 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] input = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int[] value = new int[input[0]];
+
+        for (int i = 0; i < input[0]; i++) {
+            value[i] = Integer.parseInt(br.readLine());
+        }
+
+        long money = input[1];
+        long coin = 0;
+
+        int day = 0;
+        while (day < input[0]) {
+
+            int now = value[day];
+
+            while (day < input[0] && now >= value[day]) {
+                now = value[day];
+                day++;
+            }
+
+            coin = money / now;
+            money %= now;
+
+            while (day < input[0] && now <= value[day]) {
+              
+                now = value[day];
+                day++;
+            }
+
+            money += (coin*now);
+        }
+
+        System.out.println(money);
+
+    }
+
+}
