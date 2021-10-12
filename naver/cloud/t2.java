022bae9027e89a6db31b7af293c2ec4d715bb07d
Ray <55176622+kdl9405@users.noreply.github.com>
2021-10-12 20:44:49 +0900

    1012

--- naver/cloud/t2.java
+++ naver/cloud/t2.java
@@ -1,8 +1,44 @@
 package naver.cloud;
 
+/* 
+    창문닦기
+    가능한 경로 중에서, 최대값 찾기
+        1. (0,0)부터 시작하고 오른쪽 또는 아래방향으로만 진행가능
+        2. 해당 칸에 가면 해당 값을 획득
+        3. x방향, y방향에 따라 비용 발생.
+        4. 가장 아랫줄에 도착했을 때, 최대 획득 값을 출력. 
+        
+
+    => 범위(1000*1000)가 크지 않아서, dfs로 해도 메모리,시간초과가 나지 않을것으로 판단.
+
+
+    => but.
+        1. xcost / ycost는 매번 계산할 필요 없이, 마지막 좌표값으로 한번에 계산가능!
+        2. 결국 기존 costs배열 각 칸의 최대값을 dp로 저장?
+        3. 
+        findDP(int x, int y){
+
+            if(dp[y][x] != 0){
+                return dp[y][x];
+            }
+
+            return dp[y][x] = Math.max(fidnDP(y-1,x), findDP(y,x-1))+cost[y][x];
+        }
+
+        이렇게 dp를 적용? 하면 더 좋았을 듯.
+
+
+*/
+
 public class t2 {
 
     public static void main(String[] args) {
+
+        int[][] costs = new int[10][10];
+        int xcost = 2;
+        int ycost = 2;
+
+        System.out.println(solution2(costs, xcost, ycost));
 
     }
 
@@ -16,6 +52,7 @@ public class t2 {
             max = 0;
         }
 
+        System.out.println(count);
 
         return max;
     }
@@ -23,22 +60,70 @@ public class t2 {
     static int max;
     static int dx[] = { 1, 0 };
     static int dy[] = { 0, 1 };
+    static int count = 0;
 
     static void findValue(int y, int x, int v, int[][] costs, int xcost, int ycost) {
 
-        if (y == costs.length-1 && x == costs[0].length-1) {
+        // System.out.println(y+" "+x);
+        count++;
+
+        if (y == costs.length - 1 && x == costs[0].length - 1) {
             max = Math.max(max, v);
             return;
-        }
-
-        if (y + 1 < costs.length) {
-            findValue(y + 1, x, v - ycost + costs[y + 1][x], costs, xcost, ycost);
         }
 
         if (x + 1 < costs[0].length) {
             findValue(y, x + 1, v - xcost + costs[y][x + 1], costs, xcost, ycost);
         }
 
+        if (y + 1 < costs.length) {
+            findValue(y + 1, x, v - ycost + costs[y + 1][x], costs, xcost, ycost);
+        }
+
         return;
+    }
+
+    public static int solution2(int[][] costs, int xcost, int ycost) {
+
+        max = 0;
+
+        dp = new int[costs.length][costs[0].length];
+        visit = new boolean[costs.length][costs[0].length];
+
+
+        int cost = ycost * (costs.length - 1);
+        for (int i = 0; i < costs[0].length; i++) {
+            max = Math.max(max, findDP(i, costs.length-1, costs) - cost);
+            cost += xcost;
+        }
+
+        System.out.println(count);
+
+        return max;
+    }
+
+    static int[][] dp;
+    static boolean[][] visit;
+
+
+    static int findDP(int x, int y, int[][] costs) {
+
+        count++;
+
+        if (visit[y][x]) {
+            return dp[y][x];
+        }
+
+        visit[y][x] = true;
+
+        if (y - 1 >= 0) {
+            dp[y][x] = Math.max(findDP(y - 1, x, costs), dp[y][x]);
+        }
+
+        if (x - 1 >= 0) {
+            dp[y][x] = Math.max(findDP(y, x - 1, costs), dp[y][x]);
+        }
+
+        return dp[y][x] = dp[y][x] + costs[y][x];
     }
 }
--- naver/cloud/t4.java
+++ naver/cloud/t4.java
@@ -1,5 +1,30 @@
 package naver.cloud;
 
+
+/* 
+    회의실의 사용 시간이 주어졌을때, 
+
+    가장 많은 회의실을 사용하는 시간을 구해라
+    여러개면 오름차순으로!!
+
+
+    => 
+        0. 최대시간(86400)까지 각 시간과 회의실 사용 갯수를 저장하는 int[][] 생성.
+        1. 회의실 사용의 시작시간을 기준으로 정렬
+        2. 누적합을 이용하여 최대 시간(86400)까지 그 값을 저장(회의실 사용이 종료된 것은 반영하지 못함)
+        3. 회의실 사용의 종료시간을 기준으로 정렬
+        4. 누적합을 이용하여 최대 시간(86400)까지 그 값을 빼줌(회의실 사용이 종료된 것을 반영)
+        5. 각 시간별 회의실 사용갯수가 반영된 배열을 정렬하여, 최대값을 가진 시간만을 List에 저장후 배열로 변환하여 반환.
+
+        즉, 정렬이 3번, 탐색이 3번 필요 (마지막 탐색은 최악의 경우에만 끝까지 탐색).
+    
+        
+        => 시작 시간과 종료시간을 하나의 누적합으로 한번에 측정한다면 더 좋았을 듯.
+
+        
+
+*/
+
 import java.util.*;
 
 public class t4 {
@@ -8,8 +33,14 @@ public class t4 {
 
         int[][] info = { { 1, 5 }, { 3, 5 }, { 7, 8 }};
 
-        System.out.println(solution(info));
+        // int[] ans = solution(info);
 
+        // for(int a : ans){
+        //     System.out.println(a);
+        // }
+
+        solution(info);
+        solution2(info);
     }
 
     public static int[] solution(int[][] info) {
@@ -80,6 +111,47 @@ public class t4 {
 
         System.out.println(answer);
         
+
+        return Arrays.stream(answer.toArray()).mapToInt(i -> (int)i).toArray();
+    }
+
+
+
+    public static int[] solution2(int[][] info) {
+        List<Integer> answer = new ArrayList<>();
+        
+        int[][] time = new int[10][2];
+        for (int i = 0; i < 10; i++) {
+            time[i][0] = i;
+        }
+
+        for(int[] i : info){
+            time[i[0]][1]++;
+            if(i[1]<10){
+                time[i[1]+1][1]--;
+            }
+        }
+
+        for(int i = 1; i<10; i++){
+            time[i][1] += time[i-1][1];
+        }
+        
+        Arrays.sort(time, (a,b)->{
+            if(a[1] == b[1]){
+                return a[0]-b[0];
+            }
+            return b[1] - a[1];
+        });
+
+        answer.add(time[0][0]);
+        for(int i = 1; i<time.length; i++){
+            if (time[i-1][1] > time[i][1]) {
+                break;
+            }
+            answer.add(time[i][0]);
+        }
+
+        System.out.println(answer);
 
         return Arrays.stream(answer.toArray()).mapToInt(i -> (int)i).toArray();
     }
