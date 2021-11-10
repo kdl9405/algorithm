64369813f96e6b25ddedf8df2b4cb18cd5f68ce9
Ray <55176622+kdl9405@users.noreply.github.com>
2021-11-10 21:02:16 +0900

    1110

---
+++ BOJ/BOJ2671.java
@@ -0,0 +1,28 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+    잠수함식별
+*/
+
+public class BOJ2671 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        
+
+        if (s.matches( "^(100+1+|01)+$" )) {
+        System.out.println("SUBMARINE");
+        }else{
+        System.out.println("NOISE");
+        }
+
+    }
+}
