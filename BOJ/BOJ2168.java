3502fcbef1b3d48ad27c827f8778eb5d17fdfcc4
Ray <55176622+kdl9405@users.noreply.github.com>
2021-11-29 23:49:59 +0900

    1129

---
+++ BOJ/BOJ2168.java
@@ -0,0 +1,29 @@
+package BOJ;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/* 
+    타일 위의 대각선
+*/
+public class BOJ2168 {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        
+
+    }
+
+    static int GCD(int x, int y){
+
+
+
+        return x;
+    }
+}
