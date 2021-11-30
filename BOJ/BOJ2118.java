3a52385225b8ffd9dec8624fe265af3342a9538a
Ray <55176622+kdl9405@users.noreply.github.com>
2021-11-30 22:47:45 +0900

    1130

---
+++ BOJ/BOJ2118.java
@@ -0,0 +1,52 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+
+    두 개의 탑
+
+*/
+public class BOJ2118 {
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N+1];
+
+        int sum = 0;
+
+        for(int i = 0; i<N; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        int left = 0;
+        int right = 0;
+
+        int now = arr[0];
+        int max = 0;
+        
+        while (left<=right && right <N) {
+            int t = Integer.min(now, sum-now);
+
+            max = Math.max(max, t);
+
+            if (t == now) {
+                right++;
+                now+=arr[right];
+            }else{
+                now-=arr[left];
+                left++;
+            }
+
+        }
+
+        System.out.println(max);
+    }
+}
--- BOJ/BOJ2168.java
+++ BOJ/BOJ2168.java
@@ -7,22 +7,35 @@ import java.util.StringTokenizer;
     타일 위의 대각선
 */
 public class BOJ2168 {
-    
+
     public static void main(String[] args) throws IOException {
-        
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int x = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
-        
+
+        int g = GCD(x, y);
+
+        System.out.println((x+y)-g);
 
     }
 
-    static int GCD(int x, int y){
+    static int GCD(int x, int y) {
 
+        if (x < y) {
+            int t = x;
+            x = y;
+            y = t;
+        }
 
+        while (y != 0) {
+            int t = x % y;
+            x = y;
+            y = t;
+        }
 
         return x;
     }
