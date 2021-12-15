1ff282a6cacf9ba1c0f4a1bd8314b4a6a2b284b3
Ray <55176622+kdl9405@users.noreply.github.com>
2021-12-15 19:10:54 +0900

    1214

---
+++ BOJ/BOJ1016.java
@@ -0,0 +1,70 @@
+package BOJ;
+
+/* 
+    제곱 ㄴㄴ 수
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1016 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long min = Long.parseLong(st.nextToken());
+        long max = Long.parseLong(st.nextToken());
+
+        int dif = (int) (max - min);
+        int limit = (int) Math.sqrt(max);
+
+        boolean[] check = new boolean[dif + 1];
+
+        for (long i = 2; i <= limit; i++) {
+
+            long num = i * i;
+
+            long j;
+            if (min % num == 0) {
+                j = min / num;
+            } else {
+                j = min / num + 1;
+            }
+
+            for (; j * num <= max; j++) {
+                check[(int) (j * num - min)] = true;
+            }
+
+        }
+
+        int count = 0;
+        for(int i = 0; i<=dif; i++){
+
+            if (!check[i]) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+/*
+ * 1 ~ 10
+ * 
+ * 1 X
+ * 2 X
+ * 3 X
+ * 4 4로 나눠짐
+ * 5 X
+ * 6 X
+ * 7 X
+ * 8 4로 나눠짐
+ * 9 9로 나눠짐
+ * 10 X
+ * 
+ * 
+ * 
+ */
---
+++ BOJ/BOJ11004.java
@@ -0,0 +1,32 @@
+package BOJ;
+
+/* 
+    K번째 수
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ11004 {
+    
+
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] A = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N; i++){
+            A[i]= Integer.parseInt(st.nextToken());
+        } 
+
+        Arrays.sort(A);
+
+        System.out.println(A[K-1]);
+    }
+}
