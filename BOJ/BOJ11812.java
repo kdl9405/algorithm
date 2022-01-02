5caceac3f55c8cea08ec3d702c75be919e3e22dd
Ray <55176622+kdl9405@users.noreply.github.com>
2022-01-03 00:16:35 +0900

    0102

---
+++ BOJ/BOJ11812.java
@@ -0,0 +1,52 @@
+package BOJ;
+
+
+/* 
+    K진 트리 
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ11812 {
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long N = Long.parseLong(st.nextToken());
+        long K = Long.parseLong(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        while (Q-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            long x = Long.parseLong(st.nextToken());
+            long y = Long.parseLong(st.nextToken());
+
+            if (K == 1) {
+                sb.append(Math.abs(x-y)).append("\n");
+            }else{
+
+                int d = 0;
+
+                while (x != y) {
+                    if (x > y) {
+                        x = (x+K-2)/K;
+                    }else{
+                        y = (y+K-2)/K;
+                    }
+
+                    d++;
+                }
+
+                sb.append(d).append("\n");
+            }
+        }
+
+        System.out.println(sb.toString().trim());
+
+        
+    }
+}
