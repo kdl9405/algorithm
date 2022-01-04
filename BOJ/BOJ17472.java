eb2bf5d4bd3b49fa762ed97fa494d28d3ceb5f34
Ray <55176622+kdl9405@users.noreply.github.com>
2022-01-04 21:56:01 +0900

    0104

---
+++ BOJ/BOJ17472.java
@@ -0,0 +1,173 @@
+package BOJ;
+
+/* 
+    다리 만들기 2
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ17472 {
+
+    public static void main(String[] args) throws IOException {
+
+        input();
+
+        defindLand();
+
+        searchBridge();
+
+        int answer = minBridges();
+
+        System.out.println(answer);
+
+    }
+
+    static int N, M, land;
+    static int[][] map;
+    static boolean[][] visit;
+    static int[][] move = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+    static List<int[]> bridge;
+    static int[] group;
+
+    static void input() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+    }
+
+    static void defindLand() {
+        int idx = 1;
+        visit = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 1 && !visit[i][j]) {
+                    dfsForDefine(i, j, idx);
+                    idx++;
+                }
+            }
+        }
+
+        land = idx - 1;
+    }
+
+    static void dfsForDefine(int i, int j, int idx) {
+        map[i][j] = idx;
+        visit[i][j] = true;
+
+        for (int d = 0; d < 4; d++) {
+            int x = i + move[d][0];
+            int y = j + move[d][1];
+
+            if (x < 0 || x >= N || y < 0 || y >= M) {
+                continue;
+            }
+
+            if (visit[x][y] || map[x][y] != 1) {
+                continue;
+            }
+
+            dfsForDefine(x, y, idx);
+        }
+    }
+
+    static void searchBridge() {
+
+        bridge = new ArrayList<>();
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] != 0) {
+                    queue.add(new int[] { i, j, map[i][j] });
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+
+            int[] now = queue.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int x = now[0] + move[i][0];
+                int y = now[1] + move[i][1];
+
+                int count = 0;
+                while (true) {
+                    if (x < 0 || x >= N || y < 0 || y >= M || map[x][y] == now[2]) {
+                        break;
+                    }
+
+                    if (map[x][y] > now[2]) {
+                        if (count >= 2) {
+                            bridge.add(new int[] { now[2], map[x][y], count });
+                        }
+                        break;
+                    }
+
+                    x += move[i][0];
+                    y += move[i][1];
+                    count++;
+
+                }
+
+            }
+
+        }
+
+    }
+
+    static int minBridges() {
+        int distance = 0;
+        group = new int[land + 1];
+
+        Collections.sort(bridge, (a, b) -> {
+            return a[2] - b[2];
+        });
+
+        int count = 0;
+
+        for (int[] b : bridge) {
+
+            int x = findGroup(b[0]);
+            int y = findGroup(b[1]);
+
+            if (x == y) {
+                continue;
+            }
+
+            distance += b[2];
+            group[y] = x;
+            count++;
+
+            if (count == land - 1) {
+                return distance;
+            }
+        }
+        return -1;
+
+    }
+
+    static int findGroup(int x) {
+        if (group[x] == 0) {
+            return x;
+        }
+
+        return group[x] = findGroup(group[x]);
+    }
+
+}
---
+++ BOJ/BOJ2618.java
@@ -0,0 +1,108 @@
+package BOJ;
+
+/* 
+    경찰차 
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ2618 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        W = Integer.parseInt(br.readLine());
+
+        works = new int[W + 1][2];
+        for (int i = 1; i <= W; i++) {
+            st = new StringTokenizer(br.readLine());
+            works[i][0] = Integer.parseInt(st.nextToken());
+            works[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        dp = new int[W + 1][W + 1];
+
+        int dist = findDP(0, 0);
+
+        route = new StringBuilder();
+        route.append(dist).append("\n");
+
+        getRoute(0, 0);
+
+        System.out.println(route.toString().trim());
+        
+    }
+
+    static int N, W;
+    static int[][] works;
+    static int[][] dp; // i번 사건을 j경찰차가 처리한 경우.
+    static int INF = 2000001;
+    static StringBuilder route;
+
+    static int findDP(int one, int two) {
+
+        if (one == W || two == W) {
+            return 0;
+        }
+
+        if (dp[one][two] != 0) {
+            return dp[one][two];
+        }
+
+        int next = Math.max(one, two) + 1;
+
+        int oneDis = 0;
+        int twoDis = 0;
+
+        if (one == 0) {
+            oneDis = Math.abs(1 - works[next][0]) + Math.abs(1 - works[next][1]);
+        } else {
+            oneDis = Math.abs(works[one][0] - works[next][0]) + Math.abs(works[one][1] - works[next][1]);
+        }
+
+        if (two == 0) {
+            twoDis = Math.abs(N - works[next][0]) + Math.abs(N - works[next][1]);
+        } else {
+            twoDis = Math.abs(works[two][0] - works[next][0]) + Math.abs(works[two][1] - works[next][1]);
+        }
+
+        return dp[one][two] = Math.min(oneDis + findDP(next, two), twoDis + findDP(one, next));
+
+    }
+
+    static void getRoute(int one, int two) {
+        if (one == W || two == W) {
+            return;
+        }
+
+        int next = Math.max(one, two) + 1;
+
+        int oneDis = 0;
+        int twoDis = 0;
+
+        if (one == 0) {
+            oneDis = Math.abs(1 - works[next][0]) + Math.abs(1 - works[next][1]);
+        } else {
+            oneDis = Math.abs(works[one][0] - works[next][0]) + Math.abs(works[one][1] - works[next][1]);
+        }
+
+        if (two == 0) {
+            twoDis = Math.abs(N - works[next][0]) + Math.abs(N - works[next][1]);
+        } else {
+            twoDis = Math.abs(works[two][0] - works[next][0]) + Math.abs(works[two][1] - works[next][1]);
+        }
+
+        if (dp[next][two] + oneDis < dp[one][next] + twoDis) {
+            route.append("1\n");
+            getRoute(next, two);
+        } else {
+            route.append("2\n");
+            getRoute(one, next);
+        }
+    }
+
+}
