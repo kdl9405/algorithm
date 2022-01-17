f7645996f165fe673cd3f691da88b07b2b315482
Ray <55176622+kdl9405@users.noreply.github.com>
2022-01-17 19:45:33 +0900

    0117

---
+++ BOJ/BOJ1051.java
@@ -0,0 +1,49 @@
+package BOJ;
+
+/* 
+    숫자 정사각형
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ1051 {
+
+    public static void main(String[] args) throws IOException {
+
+        new BOJ1051().solution();
+    }
+
+    private void solution() throws IOException {
+
+        // input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        char[][] rectangle = new char[N][M];
+        for (int i = 0; i < N; i++) {
+            rectangle[i] = br.readLine().toCharArray();
+        }
+
+        // findMaxSize
+        int size = 1;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                int len = Math.min(N-i, M-j);  
+
+                for(int x = 1; x<len; x++){
+                    if (rectangle[i][j] == rectangle[i][j+x] && rectangle[i][j] == rectangle[i+x][j] && rectangle[i][j] == rectangle[i+x][j+x]) {
+                        size = Math.max(size, (int)Math.pow(x+1, 2));
+                    }
+                }
+            }
+        }
+
+        System.out.println(size);
+
+    }
+
+}
