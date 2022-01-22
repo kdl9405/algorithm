0c721ebff9afc525ae48438d3ea98016a70c2383
Ray <55176622+kdl9405@users.noreply.github.com>
2022-01-23 04:12:57 +0900

    0122

---
+++ BOJ/BOJ10953.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+/*  
+A+B - 6
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ10953 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        new BOJ10953().solution();
+    }
+
+    void solution() throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine(), ",", false);
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+
+            sb.append((A+B)).append("\n");
+            
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
