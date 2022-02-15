f42305b198d72263251821a2db371d931206931c
Ray <55176622+kdl9405@users.noreply.github.com>
2022-02-15 21:59:59 +0900

    0215 BOJ

---
+++ BOJ/BOJ2331.java
@@ -0,0 +1,53 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+/* 
+    반복수열
+*/
+public class BOJ2331 {
+
+    public static void main(String[] args) throws IOException {
+
+        new BOJ2331().solution();
+    }
+
+    void solution() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(A, 0);
+
+        int index = 1;
+        while (true) {
+
+            int next = nextNum(A, P);
+            if (map.containsKey(next)) {
+                System.out.println(map.get(next));
+                return;
+            }
+
+            A = next;
+            map.put(A, index++);
+        }
+    }
+
+    int nextNum(int A, int P) {
+
+        int next = 0;
+
+        while (A > 0) {
+
+            next += Math.pow(A % 10, P);
+            A /= 10;
+        }
+
+        return next;
+    }
+}
---
+++ BOJ/BOJ9566.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+/* 
+    돌 게임 2
+*/
+
+import java.io.*;
+
+public class BOJ9566 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N % 2 == 0) {
+            System.out.println("SK");
+        }else{
+            System.out.println("CY");
+        }
+    }
+}
