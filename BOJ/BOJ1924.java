5f411f5407c34b1427d2db49d3e707855a87459c
Ray <55176622+kdl9405@users.noreply.github.com>
2022-02-17 23:36:44 +0900

    0217

---
+++ BOJ/BOJ1924.java
@@ -0,0 +1,32 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+
+/**
+ * 2007년
+ */
+public class BOJ1924 {
+
+    public static void main(String[] args) throws IOException {
+
+        int[] month = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        String[] day = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+
+        while (x-- > 0) {
+
+            y += month[x];
+
+        }
+
+        System.out.println(day[y % 7]);
+
+    }
+}
