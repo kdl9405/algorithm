1950fd351e135dd52aa7916638289f37514d6681
Ray <55176622+kdl9405@users.noreply.github.com>
2022-02-21 23:43:13 +0900

    0221

---
+++ BOJ/BOJ16194.java
@@ -0,0 +1,32 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+/* 
+    카드 구매하기 2
+*/
+
+public class BOJ16194 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[N + 1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            dp[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = i / 2; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[j] + dp[i - j]);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
