8e5e6b99d102a6d9fa53d7aee1c724e896f850be
Ray <55176622+kdl9405@users.noreply.github.com>
2022-03-04 02:18:35 +0900

    0303

---
+++ BOJ/BOJ10157.java
@@ -0,0 +1,54 @@
+package BOJ;
+
+/* 
+    자리배정
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ10157 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int C = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        int K = Integer.parseInt(br.readLine());
+
+        if (K > C * R) {
+            System.out.println(0);
+            return;
+        }
+
+        int x = 1;
+        int y = 1;
+
+        boolean[][] visit = new boolean[R + 1][C + 1];
+
+        int d = 0;
+        while (K-- > 1) {
+
+            visit[y][x] = true;
+
+            int nx = x + move[d][1];
+            int ny = y + move[d][0];
+
+            if (ny > R || ny < 1 || nx > C || nx < 1 || visit[ny][nx]) {
+                d = (d + 1) % 4;
+                nx = x + move[d][1];
+                ny = y + move[d][0];
+            }
+
+            x = nx;
+            y = ny;
+        }
+
+        System.out.println(x + " " + y);
+    }
+
+    static int[][] move = { { 1, 0 }, { 0, 1 }, { -1, 0 }, { 0, -1 } };
+}
---
+++ BOJ/BOJ3046.java
@@ -0,0 +1,21 @@
+package BOJ;
+
+/* 
+    R2
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ3046 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int R1 = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+
+        System.out.println(2*S-R1);
+    }
+}
