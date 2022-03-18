39c3cc9682b3445f2455838e21fb41f594a59934
Ray <55176622+kdl9405@users.noreply.github.com>
2022-03-18 18:41:18 +0900

    0318

---
+++ BOJ/BOJ13398.java
@@ -0,0 +1,37 @@
+package BOJ;
+
+/**
+ 연속합 2
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ13398 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] num = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[][] dp = new int[n][2];
+        dp[0][0] = num[0];
+        dp[0][1] = num[0];
+
+        int max = num[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0] + num[i], num[i]);
+            dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][1] + num[i]);
+
+            max = Math.max(max, Math.max(dp[i][0], dp[i][1]));
+        }
+
+        System.out.println(max);
+    }
+}
