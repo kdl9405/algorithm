35000615f0fb54ad9dcecf4e32f277df93f7d1a9
Ray <55176622+kdl9405@users.noreply.github.com>
2022-03-27 23:05:01 +0900

    0327

---
+++ BOJ/BOJ10159.java
@@ -0,0 +1,63 @@
+package BOJ;
+
+/* 
+    저울
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ10159 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        int[][] isKnown = new int[N + 1][N + 1];
+        StringTokenizer st;
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            isKnown[a][b] = 1;
+            isKnown[b][a] = -1;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                if (k == i) {
+                    continue;
+                }
+                for (int j = 1; j <= N; j++) {
+                    if (i == j || j == k) {
+                        continue;
+                    }
+                    if (isKnown[i][k] != 0 && isKnown[i][k] == isKnown[k][j]) {
+                        isKnown[i][j] = isKnown[i][k];
+                        isKnown[j][i] = -isKnown[i][k];
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i<=N; i++){
+            int count = 0;
+            for(int j = 1; j<=N; j++){
+
+                if (i == j) {
+                    continue;
+                }
+                if (isKnown[i][j] == 0) {
+                    count++;
+                }
+            }
+            sb.append(count).append("\n");
+        }
+    
+        System.out.println(sb.toString().trim());
+    }
+}
---
+++ BOJ/BOJ10773_copy.java
@@ -0,0 +1,33 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ10773_copy {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            sum+=stack.pop();
+        }
+
+        System.out.println(sum);
+    }
+}
