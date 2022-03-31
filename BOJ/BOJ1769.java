8ef40acfccb25d8e742552e8f39f4a29d789782a
Ray <55176622+kdl9405@users.noreply.github.com>
2022-03-31 22:01:45 +0900

    0331

---
+++ BOJ/BOJ1769.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+/* 
+
+    3의 배수
+
+*/
+
+import java.io.*;
+
+public class BOJ1769 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String number = br.readLine();
+
+        int count = 0;
+        while (number.length() > 1) {
+            count++;
+            int num = 0;
+            for (int i = 0; i < number.length(); i++) {
+                num += (number.charAt(i) - '0');
+            }
+            number = Integer.toString(num);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(count).append("\n");
+        sb.append((Integer.parseInt(number) % 3 == 0) ? "YES" : "NO");
+
+        System.out.println(sb.toString());
+    }
+}
