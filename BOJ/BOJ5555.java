985805ed12f924fb1c2bbccc5623e1bffc2d4718
Ray <55176622+kdl9405@users.noreply.github.com>
2022-04-06 23:23:27 +0900

    0406

---
+++ BOJ/BOJ5555.java
@@ -0,0 +1,37 @@
+package BOJ;
+
+/**
+    반지 
+*/
+
+import java.io.*;
+
+public class BOJ5555 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 0;
+
+        while (N-- > 0) {
+            StringBuilder ring = new StringBuilder(br.readLine());
+
+            if (ring.length() < S.length()) {
+                continue;
+            }
+
+            ring.append(ring);
+
+            if (ring.toString().contains(S)) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
