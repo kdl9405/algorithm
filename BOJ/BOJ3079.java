8939648b1bc79d9c0e1da449dde1755641513945
Ray <55176622+kdl9405@users.noreply.github.com>
2022-05-30 23:11:43 +0900

    0530

---
+++ BOJ/BOJ3079.java
@@ -0,0 +1,55 @@
+package BOJ;
+
+/* 
+    입국심사 
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ3079 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] times = new int[N];
+        int minCost = Integer.MAX_VALUE;
+        for (int i = 0; i < N; i++) {
+            times[i] = Integer.parseInt(br.readLine());
+            minCost = Math.min(minCost, times[i]);
+
+        }
+
+        long max = minCost * M;
+        long min = 0L;
+
+        long answer = max;
+
+        while (min <= max) {
+            long mid = (max + min) / 2;
+
+            int pass = 0;
+            for (int time : times) {
+                pass += mid / time;
+                if(pass>=M){
+                    break;
+                }
+            }
+
+            if (pass >= M) {
+                answer = mid;
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+}
---
+++ BOJ/BOJ3079_copy.java
@@ -0,0 +1,40 @@
+package BOJ;
+
+/* 
+    입국심사 
+
+    메모리 초과
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ3079_copy {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> {
+            return a[1] - b[1];
+        });
+
+        for (int i = 0; i < N; i++) {
+            int t = Integer.parseInt(br.readLine());
+            queue.add(new int[] { t, t });
+        }
+
+        int time = 0;
+        for (int i = 0; i < M; i++) {
+            int[] now = queue.poll();
+            time = now[1];
+            queue.add(new int[] { now[0], now[0] + now[1] });
+        }
+
+        System.out.println(time);
+    }
+}
